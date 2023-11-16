686c144739d118fb2f94c753690ebff931983891
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-11-16 17:40:45 +0330

    add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 export default async function sitemap() {
     const staticRoutes = [""];
     const routes = staticRoutes.map((route:any) => ({
-        url: `https://www.khanebetoni.com${route}`,
+        url: `https://www.tebfarda.com${route}`,
         lastModified: new Date().toString(),
     }));
     return [...routes]
